7078d5a1dd3e549b75e7d9002bc5957d34aa9ae8
tam.le.ta2001@gmail.com <tamtar1@uw.edu>
2022-07-06 21:02:39 -0700

    create histograms

--- scripts/models.R
+++ scripts/models.R
@@ -3,36 +3,49 @@
 #load library
 library(lme4)
 library(tidyverse)
+library(ggplot2)
+library(patchwork)
+library(MuMIn)
 
 #load data
 all<-read.csv("data/all.csv")
 
 #scale
-all_scale <- scale(all[8:16])
-all.mod <- data.frame("cam" = all$cam, "sex" = all$sex, "Folder" = all$Folder,
+all_scale <- scale(all[10:17])
+all.mod <- data.frame("cam" = all$cam, "coug_id" = all$coug_id, "sex" = all$sex, "Folder" = all$Folder,
                       "carcass" = all$carcass, "study_area" = all$study_area,
-                      "season" = all$season,all_scale)
+                      "season" = all$season, "total.time" = all$total.time,
+                      "nobs" = all$nobs, all_scale)
 #model
-test<-lmer(nobs ~ building_density + road_density + trail_distance +
-             forest_density + elevation + season + (1|study_area), 
+test<-lmer(nobs ~ building_density + road_density + trail_density +
+             forest_density + elevation + season + (1|coug_id), 
            data = all.mod)
-test<-lm(nobs ~ building_density + road_density + trail_distance +
+test<-lm(nobs ~ building_density + road_density + trail_density +
              forest_density + elevation + season, 
-           data = all.mod)
+           data = all.mod, na.action = "na.fail")
 test<-lm(total.time ~ building_density + road_density + trail_distance +
            forest_density + elevation + season, 
          data = all.mod)
 summary(test)
+dredge(test)
 
-#plot range of forest density
-test<-all%>%
-  subset(building_density <= 40)
-range(all$forest_density)
-range(all$building_density)
-range(all$road_density)
-hist(all$road_density)
-hist(test$building_density)
-hist(all$forest_density)
-hist(all$elevation)
-hist(test$trail_distance)
-hist(all$season)
+#histograms of predictors
+p1<-ggplot(all, aes(x=building_density))+
+  geom_histogram()
+p2<-ggplot(all, aes(x=road_density))+
+  geom_histogram()
+p3<-ggplot(all, aes(x=trail_density))+
+  geom_histogram()
+p4<-ggplot(all, aes(x=forest_density))+
+  geom_histogram()
+p5<-ggplot(all, aes(x=elevation))+
+  geom_histogram()
+
+#combine the graphs
+(p1 + p2) / (p3 + p4) / p5
+
+#compare Moultrie vs Reconyx
+ggplot(all, aes(x=nobs, color=Folder)) +
+  geom_histogram(fill="white")
+ggplot(all, aes(x=total.time, color=Folder)) +
+  geom_histogram(fill="white")
